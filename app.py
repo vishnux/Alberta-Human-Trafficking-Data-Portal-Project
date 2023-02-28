792f195df7b27addc8443a0da598e6b8f50d2b36
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-02-27 18:46:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,43 @@
 # # st.subheader(f"Total Persons Charged in {year}:")
 # # st.altair_chart(charge_chart)
 
+# import pandas as pd
+# import streamlit as st
+# import altair as alt
+
+# # Load the data
+# data = pd.read_csv("trafficking_data.csv")
+
+# # Create a dictionary for selecting options
+# options = {
+#     "Actual incidents": "Actual incidents",
+#     "Rate per 100,000 population": "Rate per 100,000 population",
+#     "Percentage change in rate": "Percentage change in rate",
+#     "Unfounded incidents": "Unfounded incidents",
+#     "Percent unfounded": "Percent unfounded"
+# }
+
+# # Create a sidebar for selecting options
+# selected_option = st.sidebar.selectbox("Select an option", list(options.keys()))
+
+# # Filter the data based on the selected option
+# filtered_data = data[data["Statistics"] == options[selected_option]]
+
+# # Create a chart
+# chart_data = filtered_data[["REF_DATE", "VALUE"]]
+# chart_data = chart_data.set_index("REF_DATE")
+# # chart_data.index = chart_data.index.astype(str)
+# chart = alt.Chart(chart_data).mark_bar().encode(
+#     x=alt.X("REF_DATE", title="Year"),
+#     y=alt.Y("VALUE", title=selected_option),
+#     tooltip=["REF_DATE", alt.Tooltip("VALUE", format=".2f")]
+# ).properties(width=700, height=400)
+
+# # Display the chart and the data table
+# st.altair_chart(chart)
+# st.write(filtered_data)
+
+
 import pandas as pd
 import streamlit as st
 import altair as alt
@@ -80,7 +117,7 @@ chart_data = filtered_data[["REF_DATE", "VALUE"]]
 chart_data = chart_data.set_index("REF_DATE")
 # chart_data.index = chart_data.index.astype(str)
 chart = alt.Chart(chart_data).mark_bar().encode(
-    x=alt.X("REF_DATE", title="Year"),
+    x=alt.X("REF_DATE:T", title="Year"),
     y=alt.Y("VALUE", title=selected_option),
     tooltip=["REF_DATE", alt.Tooltip("VALUE", format=".2f")]
 ).properties(width=700, height=400)
@@ -88,5 +125,3 @@ chart = alt.Chart(chart_data).mark_bar().encode(
 # Display the chart and the data table
 st.altair_chart(chart)
 st.write(filtered_data)
-
-
