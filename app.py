6249db76cd8afe61d43d2344d86fcd4dda38993a
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-02-27 18:55:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -142,42 +142,6 @@
 # # st.altair_chart(chart)
 # # st.write(filtered_data)
 
-# import pandas as pd
-# import streamlit as st
-# import altair as alt
-
-# # Load the data
-# data = pd.read_csv("trafficking_data.csv")
-
-# # Create a dictionary for selecting options
-# options = {
-#     "Actual incidents": "Actual incidents",
-#     "Rate per 100,000 population": "Rate per 100,000 population",
-#     "Percentage change in rate": "Percentage change in rate",
-#     "Unfounded incidents": "Unfounded incidents",
-#     "Percent unfounded": "Percent unfounded"
-# }
-
-# # Create a sidebar for selecting options
-# selected_option = st.sidebar.selectbox("Select an option", list(options.keys()))
-
-# # Filter the data based on the selected option
-# filtered_data = data[data["Statistics"] == options[selected_option]]
-
-# # Create a chart
-# chart_data = filtered_data[["REF_DATE", "VALUE"]]
-# chart_data = chart_data.set_index("REF_DATE")
-# chart_data.index = pd.to_datetime(chart_data.index)
-# chart = alt.Chart(chart_data).mark_bar().encode(
-#     x=alt.X("REF_DATE:T", title="Year"),
-#     y=alt.Y("VALUE", title=selected_option),
-#     tooltip=["REF_DATE:T", alt.Tooltip("VALUE", format=".2f")]
-# ).properties(width=700, height=400)
-
-# # Display the chart and the data table
-# st.altair_chart(chart)
-# st.write(filtered_data)
-
 import pandas as pd
 import streamlit as st
 import altair as alt
@@ -199,21 +163,21 @@ selected_option = st.sidebar.selectbox("Select an option", list(options.keys()))
 
 # Filter the data based on the selected option
 filtered_data = data[data["Statistics"] == options[selected_option]]
-
-# Convert REF_DATE to integer format
-filtered_data["REF_DATE"] = filtered_data["REF_DATE"].astype(int)
-
+st.write(filtered_data)
 # Create a chart
 chart_data = filtered_data[["REF_DATE", "VALUE"]]
 chart_data = chart_data.set_index("REF_DATE")
+chart_data.index = pd.to_datetime(chart_data.index)
 chart = alt.Chart(chart_data).mark_bar().encode(
-    x=alt.X("REF_DATE", title="Year"),
+    x=alt.X("REF_DATE:T", title="Year"),
     y=alt.Y("VALUE", title=selected_option),
-    tooltip=["REF_DATE", alt.Tooltip("VALUE", format=".2f")]
+    tooltip=["REF_DATE:T", alt.Tooltip("VALUE", format=".2f")]
 ).properties(width=700, height=400)
 
 # Display the chart and the data table
 st.altair_chart(chart)
 st.write(filtered_data)
+
+
 
 
