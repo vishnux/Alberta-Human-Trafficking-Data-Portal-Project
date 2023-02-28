d5b1215696599fe087552f4aa161259c28a13d44
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-02-27 18:44:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -77,16 +77,16 @@ filtered_data = data[data["Statistics"] == options[selected_option]]
 
 # Create a chart
 chart_data = filtered_data[["REF_DATE", "VALUE"]]
-# chart_data = chart_data.set_index("REF_DATE")
+chart_data = chart_data.set_index("REF_DATE")
 # chart_data.index = chart_data.index.astype(str)
-# chart = alt.Chart(chart_data).mark_bar().encode(
-#     x=alt.X("REF_DATE", title="Year"),
-#     y=alt.Y("VALUE", title=selected_option),
-#     tooltip=["REF_DATE", alt.Tooltip("VALUE", format=".2f")]
-# ).properties(width=700, height=400)
+chart = alt.Chart(chart_data).mark_bar().encode(
+    x=alt.X("REF_DATE", title="Year"),
+    y=alt.Y("VALUE", title=selected_option),
+    tooltip=["REF_DATE", alt.Tooltip("VALUE", format=".2f")]
+).properties(width=700, height=400)
 
-# # Display the chart and the data table
-# st.altair_chart(chart)
+# Display the chart and the data table
+st.altair_chart(chart)
 st.write(filtered_data)
 
 
